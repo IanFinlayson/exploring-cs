4674c42925cbeda825728aaecc745dc1dd75d5cc
Ian Finlayson <ifinlay@umw.edu>
2019-06-11 17:58:10 +0000

    Got code to build the navbar links automatically

--- boiler/nav.php
+++ boiler/nav.php
@@ -1,27 +1,44 @@
-
-<div class="topnav">
 <?php
-    /* get the target */
-    $target = $_GET["page"];
+    /* list of the chapters for making the nav bar */
+    $parts = array("preface", "chapter1", "chapter2");
+    $display = array("Preface", "Chapter 1", "Chapter 2");
 
-    echo "<a href=\"preface\">← Preface</a>\n";
+    /* get the target, scrubbing the leading "exploring-cs/" */
+    $target = substr($_GET["page"], strlen("exploring-cs/"));
+    $idx = array_search($target, $parts);
 
-    /* only do sections if there are some and not home page */
-    if ($target != "home" && (count($sections) > 1)) {
-        echo "<div class=\"dropdown\">\n";
-        echo "<a class=\"dropbtn\">Contents ▼</a>\n";
-        echo "<div class=\"dropdown-content\">\n";
 
-        for ($i = 0; $i < count($sections); $i++) {
-            echo "<a href=\"#$simples[$i]\">$sections[$i]</a>\n"; 
+    if ($idx !== False) {
+        echo "<div class=\"topnav\">";
+
+        /* find the back part */
+        if ($idx >= 1) {
+            $link = $parts[$idx - 1];
+            $disp = $display[$idx - 1];
+            echo "<a href=\"$link\">← $disp</a>\n";
+        }
+
+        /* only do sections if there are some and not home page */
+        if (count($sections) > 1) {
+            echo "<div class=\"dropdown\">\n";
+            echo "<a class=\"dropbtn\">Contents ▼</a>\n";
+            echo "<div class=\"dropdown-content\">\n";
+
+            for ($i = 0; $i < count($sections); $i++) {
+                echo "<a href=\"#$simples[$i]\">$sections[$i]</a>\n"; 
+            }
+            echo "</div>";
+            echo "</div>";
+        }
+
+        /* find the next part */
+        if ($idx < (count($parts) - 1)) {
+            $link = $parts[$idx + 1];
+            $disp = $display[$idx + 1];
+            echo "<a class=\"righty\" href=\"$link\">$disp →</a>\n";
         }
         echo "</div>";
-        echo "</div>";
+        echo "<div class=\"main\">";
     }
-
-    echo "<a class=\"righty\" href=\"chapter2\">Chapter 2 →</a>\n";
 ?>
-</div>
-
-<div class="main">
 
